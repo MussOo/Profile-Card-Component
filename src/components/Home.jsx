ca9d107aa92fdcf4d2447425debf817813dc7e5b
Salmi Mustapha <mustapha.salmi08000@gmail.com>
2023-05-06 17:09:19 +0200

    commit 1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,13 @@
 import style from './Home.module.scss';
+import image from '../../profile-card-component-main/images/bg-pattern-top.svg';
+import Portail_Card from './Portail_Card/Portail_Card';
 
 function Home() {
-  return <div className={style.container}>Hello world</div>;
+  return (
+    <div className={style.container}>
+      <Portail_Card />
+    </div>
+  );
 }
 
 export default Home;
---
+++ src/components/Portail_Card/Portail_Card.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import style from './Portail_Card.module.scss';
+import bg_pattern_card from '../../../public/images/bg-pattern-card.svg';
+
+export default function Portail_Card() {
+  return (
+    <div className={style.card}>
+      <div className={style.card__header}>
+        <img src={bg_pattern_card} alt='bg-pattern-top' />
+      </div>
+      <div className={style.card__body}>
+        <div className={style.card__body__image}>
+          <img src='images/image-victor.jpg' alt='image-victor' />
+        </div>
+        <div className={style.card__body__profil_info}>
+          <div>
+            <h2 className={style.card__Name__info}>
+              Victor Crest <span>26</span>
+            </h2>
+          </div>
+          <div>
+            <span>London</span>
+          </div>
+        </div>
+        <div className={style.card__body__stat}>
+          <div className={style.card__body__counter}>
+            <span>
+              <strong>80K</strong>
+            </span>
+            <span className={style.card__body__counter__label}>Follower</span>
+          </div>
+          <div className={style.card__body__counter}>
+            <span>
+              <strong>80K</strong>
+            </span>
+            <span className={style.card__body__counter__label}>Likes</span>
+          </div>
+          <div className={style.card__body__counter}>
+            <span>
+              <strong>80K</strong>
+            </span>
+            <span className={style.card__body__counter__label}>Photos</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
